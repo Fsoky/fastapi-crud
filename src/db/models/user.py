554a9bc3acb61c7ae75dfdd66ed15c7ab25e7160
Fsoky <cyberuest0x12@gmail.com>
2025-11-18 06:34:13 +0500

    User ORM Models

--- src/db/models/user.py
+++ src/db/models/user.py
@@ -3,6 +3,7 @@ from tortoise.models import Model
 
 
 class User(Model):
+    id = fields.IntField(pk=True) # Cuz mypy scream error: "User" has no attribute "id"
     created_at = fields.DatetimeField(auto_now_add=True)
     name = fields.CharField(32)
     age = fields.SmallIntField()
