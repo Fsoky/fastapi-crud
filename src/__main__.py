28ac7268d87106c62c546be05824b2e628aefd0f
Fsoky <cyberuest0x12@gmail.com>
2025-11-18 06:28:42 +0500

    Setup main file for app launch

--- src/__main__.py
+++ src/__main__.py
@@ -1,18 +1,24 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Any
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 import uvicorn
 
 from tortoise import Tortoise
 
-from api import setup_routers
-from config_reader import config, TORTOISE_ORM
+from src.api import setup_routers
+from src.core.config import config, TORTOISE_ORM
 
 
+@asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator:
     await Tortoise.init(TORTOISE_ORM)
-    yield
-    await Tortoise.close_connections()
+    
+    # Fix terminal freeze bug
+    try:
+        yield
+    finally:
+        await Tortoise.close_connections()
 
 
 app = FastAPI(lifespan=lifespan)
@@ -20,4 +26,4 @@ app.include_router(setup_routers())
 
 
 if __name__ == "__main__":
-    uvicorn.run("__main__:app", host=config.APP_HOST, port=config.APP_PORT, reload=True)
+    uvicorn.run("src.__main__:app", host=config.APP_HOST, port=config.APP_PORT, reload=True)
